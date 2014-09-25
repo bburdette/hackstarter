ed1673ea54fbbd8289eff3e8177308f032ea8a8c
Ben Burdette <bburdette@gmail.com>
2014-09-25 16:19:16 -0600

    user add form, took incredibly longer than necessary

--- Handler/Users.hs
+++ Handler/Users.hs
@@ -101,3 +101,29 @@ User
     deriving Typeable
 -}
 
+userForm :: Html -> MForm Handler (FormResult User, Widget)
+userForm = renderDivs $ User 
+  <$> areq textField "Identtttt" Nothing
+  <*> aopt passwordField "Pwd" Nothing
+
+getUserR :: Handler Html
+getUserR = do
+    (formWidget, formEnctype) <- generateFormPost userForm
+    let handlerName = "getUserR" :: Text
+    defaultLayout
+      [whamlet|
+          <p> meh here's some crappe
+          <form method=post action=@{UserR} enctype=#{formEnctype}>
+            ^{formWidget}
+            <p> some text
+            <button>Submit
+      |] 
+
+postUserR :: Handler Html
+postUserR = do
+    ((result, formWidget), formEnctype) <- runFormPost userForm
+    case result of 
+      FormSuccess user -> defaultLayout [whamlet|<p>#{show user}|]
+      _ -> defaultLayout [whamlet|Invalid|]
+
+
