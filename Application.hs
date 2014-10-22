e93477ebc9544cfda79b6f4e3e9fced9f092c60f
Ben Burdette <bburdette@gmail.com>
2014-10-22 15:51:22 -0600

    add ledger entry page

--- Application.hs
+++ Application.hs
@@ -36,6 +36,7 @@ import Handler.User
 import Handler.DuesRates
 import Handler.AddDuesRate
 import Handler.DuesRate
+import Handler.AddLedgerEntry
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/AddLedgerEntry.hs
@@ -0,0 +1,38 @@
+module Handler.AddLedgerEntry where
+
+import Import
+import Data.Time
+
+data NewLedgerEntry = NewLedgerEntry {
+  userName :: Text, 
+  amount :: Int
+  }
+  
+newLedgerEntryForm :: Maybe User -> Form NewLedgerEntry
+newLedgerEntryForm mbusr = renderDivs $ NewLedgerEntry
+  <$> areq textField ("user name: " { fsAttrs = [("readonly", "")] }) (userIdent <$> mbusr)
+  <*> areq intField "amount" Nothing
+
+getAddLedgerEntryR :: UserId -> Handler Html
+getAddLedgerEntryR uid = do
+  mbuser <- runDB $ get uid
+  case mbuser of 
+    Nothing -> error "user record not found" 
+    Just usr -> do 
+      (widget, enctype) <- generateFormPost (newLedgerEntryForm (Just usr))
+      defaultLayout $ [whamlet| 
+        <form method=post enctype#{enctype}>
+          ^{widget}
+          <input type=submit value="Add transaction">
+        |]
+      
+postAddLedgerEntryR :: UserId -> Handler Html
+postAddLedgerEntryR uid = do
+  ((result, widget), enctype) <- runFormPost $ newLedgerEntryForm Nothing
+  case result of 
+    FormSuccess nle -> do 
+      now <- lift getCurrentTime
+      blah <- runDB $ insert $ Ledger uid (amount nle) now
+      defaultLayout $ do
+        [whamlet|insert result: #{show blah}|]
+    _ -> error "fail"
