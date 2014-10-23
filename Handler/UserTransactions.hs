2303204b6cc7648d602fca85f22fb62b65de988a
Ben Burdette <bburdette@gmail.com>
2014-10-23 11:04:57 -0600

    put some listings into tables instead of just lists.

--- Handler/UserTransactions.hs
+++ Handler/UserTransactions.hs
@@ -4,14 +4,24 @@ import Import
 
 getUserTransactionsR :: UserId -> Handler Html
 getUserTransactionsR uid = do
-  -- mbusr <- runDB $ get uid
-  trannies <- runDB $ selectList [LedgerUser ==. uid] []
-  defaultLayout $ do
-    [whamlet| transactions:
-      $forall (Entity key trans) <- trannies
-        <li>
-          #{ledgerAmount trans} #{show $ ledgerDate trans}
-      |]
+  mbusr <- runDB $ get uid
+  case mbusr of 
+    Nothing -> error "invalid user id"
+    Just usr -> do 
+      ledges <- runDB $ selectList [LedgerUser ==. uid] []
+      defaultLayout $ do
+        [whamlet| 
+          <h3> transactions for user: 
+            <a href=@{UserR uid}>#{userIdent usr} 
+          <table class="low">
+            <tr>
+              <th> amount
+              <th> datetime
+            $forall (Entity key ledge) <- ledges
+              <tr>
+                <td> #{ledgerAmount ledge} 
+                <td> #{show $ ledgerDate ledge}
+          |]
 
 postUserTransactionsR :: UserId -> Handler Html
 postUserTransactionsR = error "Not yet implemented: postUserTransactionsR"
