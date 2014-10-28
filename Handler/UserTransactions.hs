b3cfd6ecf5bec8ab11ead6176f5556652aa80bcc
Ben Burdette <bburdette@gmail.com>
2014-10-28 13:17:21 -0600

    getuserbalance ftn

--- Handler/UserTransactions.hs
+++ Handler/UserTransactions.hs
@@ -4,37 +4,43 @@ import Import
 import qualified Database.Esqueleto      as E
 import           Database.Esqueleto      ((^.))
 
+getUserBalance :: UserId -> Handler Int
+getUserBalance uid = do
+  (runDB $ E.select 
+    $ E.from $ \lolwut -> do 
+      let sumamt = (E.sum_ (lolwut ^. LedgerAmount))
+      E.where_ $ lolwut ^. LedgerUser E.==. (E.val uid)
+      return sumamt) >>= (\x -> 
+        case x of 
+          [E.Value (Just amt)] -> return amt
+          _ -> return 0) 
+
 getUserTransactionsR :: UserId -> Handler Html
 getUserTransactionsR uid = do
   mbusr <- runDB $ get uid
+  bal <- getUserBalance uid
   case mbusr of 
     Nothing -> error "invalid user id"
     Just usr -> do 
-      [E.Value (Just amt)] <- runDB $ E.select 
-        $ E.from $ \lolwut -> do 
-          let sumamt = (E.sum_ (lolwut ^. LedgerAmount))
-          E.where_ $ lolwut ^. LedgerUser E.==. (E.val uid)
-          return sumamt
-      let usrsum = amt :: Int in do
-        ledges <- runDB $ selectList [LedgerUser ==. uid] []
-        defaultLayout $ do
-          [whamlet| 
-            <h3> transactions for user: 
-              <a href=@{UserR uid}>#{userIdent usr} 
-            <table class="sum">
+      ledges <- runDB $ selectList [LedgerUser ==. uid] []
+      defaultLayout $ do
+        [whamlet| 
+          <h3> transactions for user: 
+            <a href=@{UserR uid}>#{userIdent usr} 
+          <table class="sum">
+            <tr>
+              <th> balance
+            <tr>
+              <td> #{show bal}
+           <table class="low">
+            <tr>
+              <th> amount
+              <th> datetime
+            $forall (Entity key ledge) <- ledges
               <tr>
-                <th> balance
-              <tr>
-                <td> #{show usrsum}
-             <table class="low">
-              <tr>
-                <th> amount
-                <th> datetime
-              $forall (Entity key ledge) <- ledges
-                <tr>
-                  <td> #{ledgerAmount ledge} 
-                  <td> #{show $ ledgerDate ledge}
-           |]
+                <td> #{ledgerAmount ledge} 
+                <td> #{show $ ledgerDate ledge}
+         |]
 
 postUserTransactionsR :: UserId -> Handler Html
 postUserTransactionsR = error "Not yet implemented: postUserTransactionsR"
