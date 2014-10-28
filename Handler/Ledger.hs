b7f70b986d7fd640b3c06fe7233a49dfae129de7
Ben Burdette <bburdette@gmail.com>
2014-10-28 11:08:53 -0600

    add sum of transactions to ledger page, also user name.

--- Handler/Ledger.hs
+++ Handler/Ledger.hs
@@ -4,50 +4,36 @@ import Import
 import qualified Database.Esqueleto      as E
 import           Database.Esqueleto      ((^.))
 
-{-
 getLedgerR :: Handler Html
 getLedgerR = do
-  ledges <- runDB $ selectList [] []
-  defaultLayout $ do 
-    [whamlet| 
-      <h4> Ledger
-      <table class="ledger">
-        <tr>
-          <th> User 
-          <th> Amount 
-          <th> Datetime
-        $forall (Entity id ledge) <- ledges
+  [E.Value (Just frack)] <- runDB $ E.select 
+    $ E.from $ \lolwut -> do 
+      let sumamt = (E.sum_ (lolwut ^. LedgerAmount))
+      return sumamt
+  let summ = frack :: Int in do
+    ledges <- runDB $ E.select 
+      $ E.from $ \(E.InnerJoin user ledger) -> do 
+        E.on $ user ^. UserId E.==. ledger ^. LedgerUser
+        return 
+          ( user ^. UserId,
+            user ^. UserIdent, 
+            ledger ^. LedgerAmount,
+            ledger ^. LedgerDate ) 
+    defaultLayout $ do 
+      [whamlet| 
+        <h4> Ledger
+        <table class="ledgarrr">
           <tr>
-            <td> #{show $ ledgerUser ledge}
-            <td> #{ledgerAmount ledge}
-            <td> #{show $ ledgerDate ledge}
-    |]
--}
-
-getLedgerR :: Handler Html
-getLedgerR = do
-  ledges <- runDB $ E.select 
-    $ E.from $ \(E.InnerJoin user ledger) -> do 
-      E.on $ user ^. UserId E.==. ledger ^. LedgerUser
-      return 
-        ( user ^. UserId,
-          user ^. UserIdent, 
-          ledger ^. LedgerAmount,
-          ledger ^. LedgerDate ) 
-  defaultLayout $ do 
-    [whamlet| 
-      <h4> Ledger
-      <table class="ledgarrr">
-        <tr>
-          <th> User 
-          <th> Amount 
-          <th> Datetime
-        $forall (E.Value usrId, E.Value usrident, E.Value amount, E.Value datetime) <- ledges
-          <tr>
-            <td> #{ usrident }
-            <td> #{ show amount }
-            <td> #{show $ datetime}
-    |]
+            <th> User 
+            <th> Amount 
+            <th> Datetime
+          $forall (E.Value usrId, E.Value usrident, E.Value amount, E.Value datetime) <- ledges
+            <tr>
+              <td> #{ usrident }
+              <td> #{ show amount }
+              <td> #{show $ datetime}
+          <br> Sum of transactions: #{show summ}
+      |]
 
 postLedgerR :: Handler Html
 postLedgerR = error "Not yet implemented: postLedgerR"
