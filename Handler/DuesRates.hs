221deaba1377c469edec81c197212276ec6fef4f
Ben Burdette <bburdette@gmail.com>
2014-11-08 12:00:52 -0700

    readonly dues rate page for nonadmins.

--- Handler/DuesRates.hs
+++ Handler/DuesRates.hs
@@ -1,13 +1,22 @@
 module Handler.DuesRates where
 
 import Import
+import Permissions
 
 getDuesRatesR :: Handler Html
 getDuesRatesR = do 
   theRates <- runDB $ selectList [] []
-  defaultLayout $ do
-    setTitle "Dues Rates"
-    $(widgetFile "duesrates")
+  id <- requireAuthId
+  admin <- isAdmin id
+  case admin of 
+    True -> do 
+      defaultLayout $ do
+        setTitle "Dues Rates"
+        $(widgetFile "duesrates")
+    False -> do 
+      defaultLayout $ do
+        setTitle "Dues Rates"
+        $(widgetFile "duesrates_ro")
 
 postDuesRatesR :: Handler Html
 postDuesRatesR = error "Not yet implemented: postDuesRatesR"
