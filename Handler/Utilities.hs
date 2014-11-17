ee475e14acd9714d676d2e6e46d78a2fd12070de
Ben Burdette <bburdette@gmail.com>
2014-11-16 22:38:14 -0700

    parsing the transactions from paypal file.y

--- Handler/Utilities.hs
+++ Handler/Utilities.hs
@@ -5,6 +5,13 @@ import System.IO
 import Data.Conduit
 import qualified Data.Text as T
 import System.Directory
+import Data.Time.Clock
+import qualified Data.Time.Format as F
+import Text.CSV
+import qualified Data.Map as M
+import Data.List
+import Data.Char
+import System.Locale
 
 paypalDir = "paypals"
 
@@ -26,6 +33,45 @@ getUtilitiesR = do
             <input type=submit value="upload">
         |]
 
+parsePaypal :: FilePath -> IO [(M.Map String String)]
+parsePaypal fp = do
+  meh <- parseCSVFromFile fp 
+  case meh of 
+    Right csvl -> 
+      case 2 > (length csvl) of
+        True -> return []
+        False -> do 
+          let heads = fmap (dropWhile isSpace) $ head csvl
+              zipmap heads datas = M.fromList $ zip heads datas
+          return $ fmap (zipmap heads) (tail csvl)
+    Left err -> return []
+
+data Transaction = Transaction 
+  { dateTime :: UTCTime
+  , name :: Text
+  , amount :: Int
+  , email :: Text
+  , transactionId :: Text
+  }
+  deriving Show
+
+-- ppToTransaction :: (M.Map String String) -> Maybe UTCTime
+ppToTransaction :: (M.Map String String) -> Maybe Transaction
+ppToTransaction mp = do
+  date <- M.lookup "Date" mp 
+  time <- M.lookup "Time" mp
+  zone <- M.lookup "Time Zone" mp
+  datetime <- F.parseTime defaultTimeLocale "%m/%d/%Y %H:%M:%S %Z" $
+    date ++ " " ++ time ++ " " ++ zone
+  name <- fmap T.pack $ M.lookup "Name" mp 
+  famount <- fmap read $ M.lookup "Net" mp :: Maybe Float
+  let amount = truncate $ famount * 100.0
+  email <- fmap T.pack $ M.lookup "From Email Address" mp
+  transactionId <- fmap T.pack $ M.lookup "Transaction ID" mp
+  Just $ Transaction datetime name amount email transactionId
+
+-- updateFromPaypal [(Map String String)]
+
 postUtilitiesR :: Handler Html
 postUtilitiesR = do
     ((result, formWidget), formEnctype) <- runFormPost sampleForm
@@ -35,8 +81,11 @@ postUtilitiesR = do
           FormMissing -> error "form missing??"
           FormFailure meh -> error $ show meh
           FormSuccess (fi,txt) -> do
-            lift $ createDirectory paypalDir
-            lift $ fileMove fi $ paypalDir ++ "//" ++ (T.unpack txt)
+            lift $ createDirectoryIfMissing True paypalDir
+            now <- lift $ getCurrentTime
+            let fname = paypalDir ++ "//paypal" ++ (show now)-- (T.unpack txt)
+            lift $ fileMove fi fname
+            recs <- lift $ parsePaypal fname
             defaultLayout $ do
               aDomId <- newIdent
               setTitle "Welcome To Yesod!"
