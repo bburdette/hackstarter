9110f5cb712ce0cfa0d693d64f4913c1467aaadd
Ben Burdette <bburdette@gmail.com>
2014-11-24 08:19:33 -0700

    initial untested calcDues ftns.

--- Handler/UserTransactions.hs
+++ Handler/UserTransactions.hs
@@ -2,6 +2,9 @@ module Handler.UserTransactions where
 
 import Import
 import Permissions
+import Data.List
+import Data.Time.Clock
+import Data.Time.Calendar
 import qualified Database.Esqueleto      as E
 import           Database.Esqueleto      ((^.))
 
@@ -15,6 +18,75 @@ getUserBalance uid = do
         case x of 
           [E.Value (Just amt)] -> return amt
           _ -> return 0) 
+
+data DuesEntry = DuesEntry {
+  date :: UTCTime,
+  amount :: Int
+  }
+
+-- if we allowed paying any amount for dues, carrying over a balance and so forth, how would we determine the
+-- dues rates without programming them in manually?
+-- could assume 'regular' unless there is a dues amount.
+-- if there is a dues amount, that's the rate until there is another valid dues amount.
+-- ok lets do it that way.
+
+
+calcDues :: UserId -> [Int] -> Handler [DuesEntry]
+calcDues uid duesrates = do 
+  -- assuming all these are FOR dues, and are an amount in the duesrates table.   
+  ledges <- runDB $ selectList [LedgerUser ==. Just uid] [Asc LedgerDate]
+  -- let transes = filter (\(dt,amt) -> elem amt duesrates) 
+  --                     (map (\(Entity xk x) -> (ledgerDate x, ledgerAmountGross x)) ledges)
+  let transes = map (\(Entity xk x) -> (ledgerDate x, ledgerAmountGross x)) ledges
+  return $ makaDoose (filter ((<) 0) (sort duesrates)) Nothing 0 transes
+  -- return $ makeDues duesrates transes
+
+
+{-
+makeDues :: Int -> [Int] -> [(UTCTime, Int)] -> [DuesEntry]
+makeDues rate duesrates ((time, amt):xs) = makaDoose rate duesrates time amt xs 
+makeDues _ _ [] = []
+-}
+
+makaDoose :: [Int] -> (Maybe (UTCTime, Int)) -> Int -> [(UTCTime, Int)] -> [DuesEntry]
+-- if no more transactions, we're done making dues entries.
+makaDoose _ _ _ [] = []
+-- add transactions until the balance is >= one of the dues rates.  that's our initial
+-- dues rate, and dues transaction datetime.
+makaDoose duesrates Nothing balance ((time,amt):rest) = 
+  let rates = takeWhile ((>=) balance) duesrates in 
+  case rates of 
+    [] -> makaDoose duesrates Nothing (balance + amt) rest
+    ratez -> let rate = last ratez in 
+      (DuesEntry time rate) : makaDoose duesrates (Just (time, rate)) (balance - rate) rest
+
+makaDoose duesrates (Just (lasttime, lastrate)) balance dooselist = 
+  let nextdate = addMonths lasttime 2
+      (paid, future) = splitOn (\(time,_) -> time <= nextdate) dooselist
+      newbalance = foldl (+) balance (map (\(_,amt) -> amt) paid)
+      in
+    if newbalance > lastrate
+      then let ddate = addMonths lasttime 1 
+               nbal = newbalance - lastrate in 
+        (DuesEntry ddate nbal) : makaDoose duesrates (Just (ddate, nbal)) nbal future 
+      else makaDoose duesrates (Just (lasttime, lastrate)) newbalance future
+
+addMonths :: UTCTime -> Integer -> UTCTime
+addMonths start months =
+  start { utctDay = addGregorianMonthsClip months (utctDay start) } 
+
+splitOn :: (a -> Bool) -> [a] -> ([a], [a])
+splitOn cond lst = 
+  spuliton cond [] lst 
+
+spuliton :: (a -> Bool) -> [a] -> [a] -> ([a],[a])
+spuliton _ [] frnt = (reverse frnt, [])
+spuliton cond (s:ss) frnt = 
+  if (cond s) then
+    (reverse frnt, (s:ss))
+  else
+    spuliton cond ss (s:frnt)
+
 
 getUserTransactionsR :: UserId -> Handler Html
 getUserTransactionsR uid = do
