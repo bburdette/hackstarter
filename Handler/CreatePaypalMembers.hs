a4e1019212adf988e87f6afa88353244a42f5b10
Ben B <bburdette@gmail.com>
2015-03-21 22:50:27 -0600

    partially working query for paypal user create.

--- Handler/CreatePaypalMembers.hs
+++ Handler/CreatePaypalMembers.hs
@@ -10,6 +10,7 @@ import           Database.Esqueleto      ((^.))
     that are in paypal transactions
       that are TO an email that belongs to the club
     that are NOT in account_email already
+    show only one of each
 
   does account need to belong to a club as well?  
     I think yes.  
@@ -30,8 +31,39 @@ userMakeForm :: [(Text,PaypalId)] -> Maybe UserMake -> Form UserMake
 userMakeForm choisez mbum = renderDivs $ UserMake 
   <$> areq (checkboxesFieldList choisez) "valid users?" (paypals <$> mbum)
 
-
+{-
+  restoo <- runDB $ E.select $ E.from $ 
+    (\(E.LeftOuterJoin paypal email) -> do
+      E.where_ $ paypal ^. PaypalFromemail E.==. email E.?. EmailId
+      E.where_ $ E.notExists $ 
+        E.from $ \accountemail -> do
+          E.where_ (paypal ^. PaypalFromemail E.==. 
+                    accountemail E.?. AccountEmailEmail)
+      E.where_ $ E.in_ (paypal E.^. PaypalToemail) $ 
+        E.select $ E.from $ \(E.InnerJoin clubaccount aeml) -> do 
+          E.where_ $ clubaccount ^. ClubAccountClub E.==. (E.val cid)
+          E.where_ $ clubaccount ^. ClubAccountAccount E.==. aeml ^. AccountEmailAccount
+          return (aeml ^. AccountEmailEmail)
+      return (paypal ^. PaypalName, paypal ^. PaypalId, email E.?. EmailEmail)) 
+ 
+  restoo <- runDB $ E.select $ E.from $ 
+    (\(E.LeftOuterJoin paypal email) -> do
+      E.where_ $ paypal ^. PaypalFromemail E.==. email E.?. EmailId
+      E.where_ $ E.notExists $ 
+        E.from $ \accountemail -> do
+          E.where_ (paypal ^. PaypalFromemail E.==. 
+                    accountemail E.?. AccountEmailEmail)
+      E.where_ $ E.exists $ 
+        E.from $ \(E.InnerJoin clubaccount aeml) -> do 
+          E.where_ $ clubaccount ^. ClubAccountClub E.==. (E.val cid)
+          E.where_ $ clubaccount ^. ClubAccountAccount E.==. aeml ^. AccountEmailAccount
+          E.where_ (paypal ^. PaypalToemail E.==. 
+                    aeml ^. AccountEmailEmail)
+      return (paypal ^. PaypalName, paypal ^. PaypalId, email E.?. EmailEmail)) 
+ -}
 getCreatePaypalMembersR :: ClubId -> Handler Html
+--getCreatePaypalMembersR cid = error "blah"
+
 getCreatePaypalMembersR cid = do  
   restoo <- runDB $ E.select $ E.from $ 
     (\(E.LeftOuterJoin paypal email) -> do
@@ -40,6 +72,14 @@ getCreatePaypalMembersR cid = do
         E.from $ \accountemail -> do
           E.where_ (paypal ^. PaypalFromemail E.==. 
                     accountemail E.?. AccountEmailEmail)
+      E.where_ $ E.exists $ 
+        E.from $ \aeml -> do 
+          E.where_ (paypal ^. PaypalToemail E.==. 
+                    aeml E.?. AccountEmailEmail)
+          E.where_ $ E.in_ (aeml E.?. AccountEmailAccount) $
+            E.subList_select $ E.from $ \clubaccount -> do
+              E.where_ $ clubaccount ^. ClubAccountClub E.==. (E.val cid)
+              return $ E.just $ clubaccount ^. ClubAccountAccount
       return (paypal ^. PaypalName, paypal ^. PaypalId, email E.?. EmailEmail)) 
   let chex = (\(E.Value name, E.Value ppid, E.Value email) -> 
                (name, ppid)) <$> restoo
@@ -66,6 +106,13 @@ getCreatePaypalMembersR cid = do
             $of (E.Value Nothing)
               <td> 
    |]
+
+{-
+
+select email.email from account_email, club_account, email where club_account.club == 1 and club_account.account == account_email.account and email.id == account_email.email;
+
+-}
+
 
 {-
 FormFailure err -> error $ show err
