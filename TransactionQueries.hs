eaa6d00577f55ceab27926c753c5947e31484ea7
Ben B <bburdette@gmail.com>
2015-04-25 12:22:13 -0600

    use unqualified import of esqueleto

--- TransactionQueries.hs
+++ TransactionQueries.hs
@@ -1,31 +1,26 @@
 module TransactionQueries where
  
-import Import
---import Permissions
---import Data.List
+import Import hiding ((==.), (!=.), (||.))
+import Database.Esqueleto;
+--import qualified Database.Persist as P
 import Data.Fixed
---import Data.Text
 import Data.Time.Clock
 import Data.Text.Internal.Search as S
 import qualified Data.Maybe as MB
 import qualified Data.Text as T
--- import Data
---import Data.Time.Calendar
-import qualified Database.Esqueleto      as E
-import           Database.Esqueleto      ((^.))
 
--- get the first user account with 'dues' in the name.  
+-- get the first user account with 'dues' in the nam  
 -- hopefully we'll replace this lame-o mechanism soon.
 getDuesAccount :: UserId -> Handler (Maybe AccountId)
 getDuesAccount uid = do 
-  rawaccts <- runDB $ E.select $ E.from (\account -> do 
-    E.where_ $ E.in_ (account ^. AccountId) (E.subList_select $ E.from 
+  rawaccts <- runDB $ select $ from (\account -> do 
+    where_ $ in_ (account ^. AccountId) (subList_select $ from 
       (\useracct -> do 
-         E.where_ $ useracct ^. UserAccountUser E.==. E.val uid
+         where_ $ useracct ^. UserAccountUser ==. val uid
          return $ useracct ^. UserAccountAccount))
     return (account ^. AccountId, account ^. AccountName))
   -- does one account contain 'dues' in the name?
-  let accts = (\(E.Value acctid, E.Value acctname) -> (acctid, acctname)) <$> rawaccts
+  let accts = (\(Value acctid, Value acctname) -> (acctid, acctname)) <$> rawaccts
       duesaccts = 
         filter (\(id,name) -> (not . null) (S.indices "dues" (T.toLower name))) accts
   return $ fst <$> MB.listToMaybe duesaccts
@@ -36,12 +31,12 @@ getDuesAccount uid = do
 
 getPaypals :: Handler [(PaypalId, UTCTime, Centi, Centi, Text, Text, Text, Maybe Text, Maybe Text, UserId, Text)]
 getPaypals = do 
-  paypals <- runDB $ E.select $ E.from $ 
-    \(E.InnerJoin (E.LeftOuterJoin (E.LeftOuterJoin paypal email) email2) usercreator) -> do 
-      E.on $ usercreator ^. UserId E.==. paypal ^. PaypalCreator
-      E.on (paypal ^. PaypalToemail E.==. email2 E.?. EmailId)
-      E.on (paypal ^. PaypalFromemail E.==. email E.?. EmailId)
-      E.orderBy $ [E.asc ( paypal ^. PaypalDate)]
+  paypals <- runDB $ select $ from $ 
+    \(InnerJoin (LeftOuterJoin (LeftOuterJoin paypal email) email2) usercreator) -> do 
+      on $ usercreator ^. UserId ==. paypal ^. PaypalCreator
+      on (paypal ^. PaypalToemail ==. email2 ?. EmailId)
+      on (paypal ^. PaypalFromemail ==. email ?. EmailId)
+      orderBy $ [asc ( paypal ^. PaypalDate)]
       return 
         ( paypal ^. PaypalId,
           paypal ^. PaypalDate,
@@ -50,19 +45,19 @@ getPaypals = do
           paypal ^. PaypalName,
           paypal ^. PaypalDescription,
           paypal ^. PaypalMemo,
-          email E.?. EmailEmail,
-          email2 E.?. EmailEmail,
+          email ?. EmailEmail,
+          email2 ?. EmailEmail,
           paypal ^. PaypalCreator,
           usercreator ^. UserIdent ) 
-  return $ (\(E.Value a, E.Value b, E.Value c, E.Value d, E.Value e, E.Value f, E.Value g, E.Value h, E.Value i, E.Value j, E.Value k) -> (a,b,c,d,e,f,g,h,i,j,k)) <$> paypals
+  return $ (\(Value a, Value b, Value c, Value d, Value e, Value f, Value g, Value h, Value i, Value j, Value k) -> (a,b,c,d,e,f,g,h,i,j,k)) <$> paypals
 
 getPaypalInternal :: Handler [(PaypalId, AccountId, Text, UserId, Text, UTCTime, Centi, Bool)]
 getPaypalInternal = do 
-  ppis <- runDB $ E.select $ E.from $
-    \(E.InnerJoin (E.InnerJoin ppi user) account) -> do
-      E.on $ ppi ^. PaypalInternalToaccount E.==. account ^. AccountId 
-      E.on $ ppi ^. PaypalInternalCreator E.==. user ^. UserId
-      E.orderBy [E.asc (ppi ^. PaypalInternalDate)]
+  ppis <- runDB $ select $ from $
+    \(InnerJoin (InnerJoin ppi user) account) -> do
+      on $ ppi ^. PaypalInternalToaccount ==. account ^. AccountId 
+      on $ ppi ^. PaypalInternalCreator ==. user ^. UserId
+      orderBy [asc (ppi ^. PaypalInternalDate)]
       return (ppi ^. PaypalInternalFrompaypal, 
               ppi ^. PaypalInternalToaccount,
               account ^. AccountName,
@@ -71,16 +66,16 @@ getPaypalInternal = do
               ppi ^. PaypalInternalDate,
               ppi ^. PaypalInternalAmount,
               ppi ^. PaypalInternalManual)
-  return $ (\(E.Value a, E.Value b, E.Value c, E.Value d, E.Value e, E.Value f, E.Value g, E.Value h) -> (a,b,c,d,e,f,g,h)) <$> ppis 
+  return $ (\(Value a, Value b, Value c, Value d, Value e, Value f, Value g, Value h) -> (a,b,c,d,e,f,g,h)) <$> ppis 
 
 getInternals :: Handler [(InternalId, AccountId, Text, AccountId, Text, UserId, Text, UTCTime, Centi, Bool)]
 getInternals = do 
-  internals <- runDB $ E.select $ E.from $ 
-    \(E.InnerJoin (E.InnerJoin (E.InnerJoin internal user) frmacct) toacct) -> do
-      E.on $ internal ^. InternalToaccount E.==. toacct ^. AccountId 
-      E.on $ internal ^. InternalFromaccount E.==. frmacct ^. AccountId
-      E.on $ internal ^. InternalCreator E.==. user ^. UserId
-      E.orderBy [E.asc $ internal ^. InternalDate]
+  internals <- runDB $ select $ from $ 
+    \(InnerJoin (InnerJoin (InnerJoin internal user) frmacct) toacct) -> do
+      on $ internal ^. InternalToaccount ==. toacct ^. AccountId 
+      on $ internal ^. InternalFromaccount ==. frmacct ^. AccountId
+      on $ internal ^. InternalCreator ==. user ^. UserId
+      orderBy [asc $ internal ^. InternalDate]
       return (internal ^. InternalId, 
               internal ^. InternalFromaccount, 
               frmacct ^. AccountName,
@@ -91,7 +86,7 @@ getInternals = do
               internal ^. InternalDate,
               internal ^. InternalAmount,
               internal ^. InternalManual)
-  return $ (\(E.Value a, E.Value b, E.Value c, E.Value d, E.Value e, E.Value f, E.Value g, E.Value h, E.Value i, E.Value j) -> (a,b,c,d,e,f,g,h,i,j)) <$> internals 
+  return $ (\(Value a, Value b, Value c, Value d, Value e, Value f, Value g, Value h, Value i, Value j) -> (a,b,c,d,e,f,g,h,i,j)) <$> internals 
 
 ----------------------------------------------------------
 -- get transactions for one user.
@@ -100,22 +95,22 @@ getInternals = do
 getAccountPaypals :: AccountId -> Handler [(PaypalId, UTCTime, Centi, Centi, Text, Text, Text, Maybe Text, Maybe Text, UserId, Text)]
 getAccountPaypals acctid = do
   -- there should be a better way than repeating that sublist query!
-  paypals <- runDB $ E.select $ E.from $ 
-    \(E.InnerJoin (E.LeftOuterJoin (E.LeftOuterJoin paypal email) email2) usercreator) -> do 
-      E.on $ usercreator ^. UserId E.==. paypal ^. PaypalCreator
-      E.on (paypal ^. PaypalToemail E.==. email2 E.?. EmailId)
-      E.on (paypal ^. PaypalFromemail E.==. email E.?. EmailId)
-      E.where_ $ (E.in_ (paypal ^. PaypalFromemail) 
-                       (E.subList_select $ E.from 
+  paypals <- runDB $ select $ from $ 
+    \(InnerJoin (LeftOuterJoin (LeftOuterJoin paypal email) email2) usercreator) -> do 
+      on $ usercreator ^. UserId ==. paypal ^. PaypalCreator
+      on (paypal ^. PaypalToemail ==. email2 ?. EmailId)
+      on (paypal ^. PaypalFromemail ==. email ?. EmailId)
+      where_ $ (in_ (paypal ^. PaypalFromemail) 
+                       (subList_select $ from 
                          (\acctemail -> do 
-                           E.where_ $ acctemail ^. AccountEmailAccount E.==. (E.val acctid)
-                           return $ E.just (acctemail ^. AccountEmailEmail))))
-           E.||. (E.in_ (paypal ^. PaypalToemail) 
-                       (E.subList_select $ E.from 
+                           where_ $ acctemail ^. AccountEmailAccount ==. (val acctid)
+                           return $ just (acctemail ^. AccountEmailEmail))))
+           ||. (in_ (paypal ^. PaypalToemail) 
+                       (subList_select $ from 
                          (\acctemail -> do 
-                           E.where_ $ acctemail ^. AccountEmailAccount E.==. (E.val acctid)
-                           return $ E.just (acctemail ^. AccountEmailEmail))))
-      E.orderBy $ [E.asc ( paypal ^. PaypalDate)]
+                           where_ $ acctemail ^. AccountEmailAccount ==. (val acctid)
+                           return $ just (acctemail ^. AccountEmailEmail))))
+      orderBy $ [asc ( paypal ^. PaypalDate)]
       return 
         ( paypal ^. PaypalId,
           paypal ^. PaypalDate,
@@ -124,20 +119,20 @@ getAccountPaypals acctid = do
           paypal ^. PaypalName,
           paypal ^. PaypalDescription,
           paypal ^. PaypalMemo,
-          email E.?. EmailEmail,
-          email2 E.?. EmailEmail,
+          email ?. EmailEmail,
+          email2 ?. EmailEmail,
           paypal ^. PaypalCreator,
           usercreator ^. UserIdent ) 
-  return $ (\(E.Value a, E.Value b, E.Value c, E.Value d, E.Value e, E.Value f, E.Value g, E.Value h, E.Value i, E.Value j, E.Value k) -> (a,b,c,d,e,f,g,h,i,j,k)) <$> paypals
+  return $ (\(Value a, Value b, Value c, Value d, Value e, Value f, Value g, Value h, Value i, Value j, Value k) -> (a,b,c,d,e,f,g,h,i,j,k)) <$> paypals
 
 getAccountPaypalInternal :: AccountId -> Handler [(PaypalId, AccountId, Text, UserId, Text, UTCTime, Centi, Bool)]
 getAccountPaypalInternal aid = do 
-  ppis <- runDB $ E.select $ E.from $
-    \(E.InnerJoin (E.InnerJoin ppi user) account) -> do
-      E.on $ ppi ^. PaypalInternalToaccount E.==. account ^. AccountId 
-      E.on $ ppi ^. PaypalInternalCreator E.==. user ^. UserId
-      E.where_ $ ppi ^. PaypalInternalToaccount E.==. (E.val aid)
-      E.orderBy [E.asc $ ppi ^. PaypalInternalDate]
+  ppis <- runDB $ select $ from $
+    \(InnerJoin (InnerJoin ppi user) account) -> do
+      on $ ppi ^. PaypalInternalToaccount ==. account ^. AccountId 
+      on $ ppi ^. PaypalInternalCreator ==. user ^. UserId
+      where_ $ ppi ^. PaypalInternalToaccount ==. (val aid)
+      orderBy [asc $ ppi ^. PaypalInternalDate]
       return (ppi ^. PaypalInternalFrompaypal, 
               ppi ^. PaypalInternalToaccount,
               account ^. AccountName,
@@ -146,18 +141,18 @@ getAccountPaypalInternal aid = do
               ppi ^. PaypalInternalDate,
               ppi ^. PaypalInternalAmount,
               ppi ^. PaypalInternalManual)
-  return $ (\(E.Value a, E.Value b, E.Value c, E.Value d, E.Value e, E.Value f, E.Value g, E.Value h) -> (a,b,c,d,e,f,g,h)) <$> ppis 
+  return $ (\(Value a, Value b, Value c, Value d, Value e, Value f, Value g, Value h) -> (a,b,c,d,e,f,g,h)) <$> ppis 
 
 getAccountInternals :: AccountId -> Handler [(InternalId, AccountId, Text, AccountId, Text, UserId, Text, UTCTime, Centi, Bool)]
 getAccountInternals aid = do 
-  internals <- runDB $ E.select $ E.from $ 
-    \(E.InnerJoin (E.InnerJoin (E.InnerJoin internal user) frmacct) toacct) -> do
-      E.on $ internal ^. InternalToaccount E.==. toacct ^. AccountId 
-      E.on $ internal ^. InternalFromaccount E.==. frmacct ^. AccountId
-      E.on $ internal ^. InternalCreator E.==. user ^. UserId
-      E.where_ $ (internal ^. InternalToaccount E.==. (E.val aid)) 
-        E.||. (internal ^. InternalFromaccount E.==. (E.val aid))
-      E.orderBy [E.asc $ internal ^. InternalDate]
+  internals <- runDB $ select $ from $ 
+    \(InnerJoin (InnerJoin (InnerJoin internal user) frmacct) toacct) -> do
+      on $ internal ^. InternalToaccount ==. toacct ^. AccountId 
+      on $ internal ^. InternalFromaccount ==. frmacct ^. AccountId
+      on $ internal ^. InternalCreator ==. user ^. UserId
+      where_ $ (internal ^. InternalToaccount ==. (val aid)) 
+        ||. (internal ^. InternalFromaccount ==. (val aid))
+      orderBy [asc $ internal ^. InternalDate]
       return (internal ^. InternalId, 
               internal ^. InternalFromaccount, 
               frmacct ^. AccountName,
@@ -168,5 +163,6 @@ getAccountInternals aid = do
               internal ^. InternalDate,
               internal ^. InternalAmount,
               internal ^. InternalManual)
-  return $ (\(E.Value a, E.Value b, E.Value c, E.Value d, E.Value e, E.Value f, E.Value g, E.Value h, E.Value i, E.Value j) -> (a,b,c,d,e,f,g,h,i,j)) <$> internals 
+  return $ (\(Value a, Value b, Value c, Value d, Value e, Value f, Value g, Value h, Value i, Value j) -> (a,b,c,d,e,f,g,h,i,j)) <$> internals 
+
 
